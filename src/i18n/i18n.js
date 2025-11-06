f3171b3c8e91d3ed83c1e79d308d58f67ecfb99d
gwendalminguy <gwendalminguy@outlook.fr>
2025-11-06 13:02:44 +0100

    Add file

---
+++ src/i18n/i18n.js
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import en from "./en.json";
+import fr from "./fr.json";
+import es from "./es.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      fr: { translation: fr },
+      es: { translation: es },
+    },
+    fallbackLng: "en",
+    supportedLngs: ["en", "fr", "es"],
+    load: "languageOnly",
+    returnNull: false,
+    detection: {
+      order: ["localStorage", "navigator", "htmlTag", "cookie"],
+      caches: ["localStorage"],
+      lookupLocalStorage: "i18nextLng",
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
